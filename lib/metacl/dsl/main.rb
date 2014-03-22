1e1f18d41eae744b622084b6d2169af87dbc2746
Roman Kolesnev <rvkolesnev@gmail.com>
2014-03-22 04:30:41 +0400

    Create and destroy matrices

--- lib/metacl/dsl/main.rb
+++ lib/metacl/dsl/main.rb
@@ -9,15 +9,28 @@ module MetaCL
       def initialize(&block)
         @code = ""
         instance_eval &block if block_given?
-        @code = Utils.apply_template 'wrapper', @config[:lang], code: Utils.tab_text(@code)
+        @code = Utils.apply_template 'wrapper', @lang, code: Utils.tab_text(@code)
       end
 
       def configure(&block)
         @config = Configure.new(&block).config
+        @lang   = @config[:lang]
       end
 
       def print_s(string)
         @code << "printf(\"#{string.gsub '"', '\"'}\\n\");\n"
+      end
+
+      def create_matrix(name, type, n, m)
+        @code << Utils.apply_template('create_matrix', @lang,
+                                      name: name,
+                                      type: type,
+                                      n: n,
+                                      m: m) << "\n"
+      end
+
+      def destroy_matrix(name)
+        @code << Utils.apply_template('destroy_matrix', @lang, name: name) << "\n"
       end
     end
   end
