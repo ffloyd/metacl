3e5a79ddd1bc83525236cf8df0f4065e17c1eb7e
Roman Kolesnev <rvkolesnev@gmail.com>
2014-03-22 15:34:05 +0400

    Matrix plus and minus, matrix printing

--- lib/metacl/dsl/matrix.rb
+++ lib/metacl/dsl/matrix.rb
@@ -1,3 +1,5 @@
+require 'metacl/utils'
+
 module MetaCL
   module DSL
     module Matrix
@@ -35,13 +37,94 @@ module MetaCL
         @code << Utils.apply_template('destroy_matrix', @lang, name: name) << "\n"
         @matrices.delete name_sym
       end
+
+      def fill_matrix_with(name, &block)
+        name_sym = name.to_sym
+        raise "Cannot fill nonexistent matrix with name '#{name_sym}'." unless @matrices.has_key? name_sym
+        @code << MatrixFiller.new(@lang, name_sym, @matrices, &block).code
+      end
+
+      def print_matrix(name)
+        name_sym = name.to_sym
+        @code << Utils.apply_template('print_matrix', @lang,
+                                      name: name_sym,
+                                      n: @matrices[name_sym].n,
+                                      m: @matrices[name_sym].m) << "\n"
+      end
     end
 
     class MatrixObject
       attr_reader :name, :type, :n, :m
+      attr_writer :code_context
 
-      def initialize(*args)
-        @name, @type, @n, @m = *args
+      def initialize(name, type, n, m)
+        @name, @type, @n, @m = name, type, n, m
+      end
+
+      def value_in_context
+        "#{@name}[#{@code_context.n_iterator} + #{n}*#{@code_context.m_iterator}]"
+      end
+
+      def +(operand)
+        VirtualMatrixObject.new "(#{value_in_context} + #{operand.value_in_context})"
+      end
+
+      def -(operand)
+        VirtualMatrixObject.new "(#{value_in_context} - #{operand.value_in_context})"
+      end
+    end
+
+    class VirtualMatrixObject
+      attr_reader :value_in_context
+
+      def initialize(value)
+        @value_in_context = value
+      end
+
+      def +(operand)
+        VirtualMatrixObject.new "(#{value_in_context} + #{operand.value_in_context})"
+      end
+
+      def -(operand)
+        VirtualMatrixObject.new "(#{value_in_context} - #{operand.value_in_context})"
+      end
+    end
+
+    class MatrixExpressionContext
+      attr_reader :used_iterators, :code, :n_iterator, :m_iterator
+
+      def initialize(n_iterator, m_iterator)
+        @used_iterators, @code, @n_iterator, @m_iterator = 0, '', n_iterator, m_iterator
+      end
+    end
+
+    class MatrixFiller
+      attr_reader :code
+
+      def initialize(lang, name, matrices, &block)
+        @lang, @name, @matrices, @code = lang, name.to_sym, matrices, ''
+        @context = MatrixExpressionContext.new(:i, :j)
+        raw_result = instance_eval(&block)
+        result  = if raw_result.respond_to? :value_in_context
+                    raw_result.value_in_context
+                  else
+                    raw_result
+                  end
+        @code << Utils.apply_template('matrix_filler', @lang,
+                                      n_iterator: 'i',
+                                      m_iterator: 'j',
+                                      matrix_name: name,
+                                      n: @matrices[name].n,
+                                      m: @matrices[name].m,
+                                      expression: result) << "\n"
+      end
+
+      def method_missing(name, *args)
+        if name =~ /\Am_/ and @matrices.has_key?(matrix_name = name[2..-1].to_sym)
+          @matrices[matrix_name].tap { |m| m.code_context = @context}
+        else
+          super
+        end
       end
     end
   end
